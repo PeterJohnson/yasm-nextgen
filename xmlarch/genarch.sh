d1686871a1fff694cfdf5c7e917332e9368d9d2e
Peter Johnson <peter@tortall.net>
2011-09-17 15:46:19 -0700

    Take another cut at instruction table description.

--- xmlarch/genarch.sh
+++ xmlarch/genarch.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-./insns2xml.py $1insns.tab > $1insns.xml
 xsltproc --xinclude --nonet nocomment.xsl $1.xml | \
 	xsltproc --nonet --output $1-full.xml arch.xsl -
 rnv arch.rnc $1-full.xml
